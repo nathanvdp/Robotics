1e195363bfedca388cc81c4bfc3eb77b037d3f23
Jesse Jonathan van der Putten <N_vdp94@hotmail.com>
2020-03-02 16:46:43 +0100

    implement server socket and leap listener

---
+++ MainAssignment/LeapMotion.py
@@ -0,0 +1,100 @@
+import Leap, sys, thread, time
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+import socket
+
+print("Waiting for connection before starting..")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("172.20.10.2", 1234))
+s.listen(2)
+
+while True:
+    clientsocket, address = s.accept()
+    print "Connection from " + str(address) + " has been established!"
+    #clientsocket.send(bytes("welcome to the server!"))
+    if clientsocket != 0:
+        break
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+    
+    def on_init(self, controller):
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+
+        # Get hands
+        for hand in frame.hands:
+            if hand.palm_position[0] < -80:
+                #print "left"
+                clientsocket.send(bytes("L"))
+            elif hand.palm_position[0] > 80:
+                #print "right"
+                clientsocket.send(bytes("R"))
+            else:
+                print "middle"
+            
+            if hand.palm_position[1]< 100:
+                clientsocket.send(bytes("X"))
+                clientsocket.close()
+                exit()
+
+            # Get the hand's normal vector and direction
+            normal = hand.palm_normal
+            direction = hand.direction
+        time.sleep(0.1)
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+
+def main():
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
